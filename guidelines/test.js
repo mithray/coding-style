964697d2fa1aa30b740d1af95f8e05b2c18828a2
mithray <mithray@protonmail.com>
2019-01-28 18:52:03 +1300

    command line complexity npm script working

--- guidelines/test.js
+++ guidelines/test.js
@@ -3,21 +3,39 @@ const fs 			= require('fs')
 const jsome 		= require('jsome');
 const path			= require('path');
 
-const source		= fs.readFileSync(path.join(__dirname, '../node_modules/tweetnacl/nacl-fast.js'), 'utf8')
-const result 		= escomplex.analyse(source);
+var source_path
 
-let halstead = result.aggregate.halstead
+function analyze(source_path){
+	const source		= fs.readFileSync( source_path , 'utf8')
+	const result 		= escomplex.analyse(source);
 
-let summary = {
-    'length': halstead.length,
-    'vocabulary': halstead.vocabulary,
-    'difficulty': halstead.difficulty,
-    'volume': halstead.volume,
-    'effort': halstead.effort,
-    'bugs': halstead.bugs,
-    'time_seconds': halstead.time,
-    'time_hours': halstead.time / 3600
+	let halstead = result.aggregate.halstead
+
+	let summary = {
+	    'length': halstead.length,
+	    'vocabulary': halstead.vocabulary,
+	    'difficulty': halstead.difficulty,
+	    'volume': halstead.volume,
+	    'effort': halstead.effort,
+	    'bugs': halstead.bugs,
+	    'time_seconds': halstead.time,
+	    'time_hours': halstead.time / 3600
+	}
+
+	jsome(summary);
 }
 
-jsome(summary);
+let args  = process.argv
+
+if (process.argv.length >= 3){
+	let arg_path = args[2]
+	console.log(arg_path)
+	source_path = arg_path
+	analyze(source_path)
+}
+else {
+	source_path = path.join(__dirname, '../node_modules/tweetnacl/nacl-fast.js')
+	analyze(source_path)
+}
+
 
